b2cee3c0fa3e9f458455f2304e9929633753d816
abdullah <60715190+abdullahbodur@users.noreply.github.com>
2026-09-06 13:23:00 +0200

    Revert "feat(navigation): define typed capability outcomes HORO-1226 #1226 [NAV-001.3]"
    This reverts commit 90d03aa25df12363f3ad15c5c34503ce6c423303.

--- cmake/HoroPublicHeaderOwnership.cmake
+++ cmake/HoroPublicHeaderOwnership.cmake
@@ -155,10 +155,8 @@ horo_configure_target_header_boundary(HoroPhysics PUBLIC_HEADERS
     Horo/Physics/PhysicsWorldSettings.h
 )
 horo_configure_target_header_boundary(HoroNavigationApi PUBLIC_HEADERS
-    Horo/Navigation/NavigationCapabilities.h
     Horo/Navigation/NavigationErrors.h
     Horo/Navigation/NavigationIdentity.h
-    Horo/Navigation/NavigationOutcomes.h
 )
 
 horo_configure_target_header_boundary(HoroRenderApi PUBLIC_HEADERS
