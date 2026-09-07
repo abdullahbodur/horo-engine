1df8b4b371a9630e5c15ad1d0e690d2680641fff
Abdullah Bodur <abdullahbodur.abbdr@gmail.com>
2026-09-07 02:12:29 +0200

    feat(save): define product user profile save namespaces HORO-1423 #1423 [SAV-003.3]

--- cmake/HoroPublicHeaderOwnership.cmake
+++ cmake/HoroPublicHeaderOwnership.cmake
@@ -68,6 +68,7 @@ horo_configure_target_header_boundary(HoroRuntime PUBLIC_HEADERS
     Horo/Runtime/Save/SaveErrors.h
     Horo/Runtime/Save/SaveIdentity.h
     Horo/Runtime/Save/SaveRootResolver.h
+    Horo/Runtime/Save/SaveNamespace.h
     Horo/Runtime/Save/SaveParticipantRegistry.h
     Horo/Runtime/Save/SaveArchiveMetadata.h
     Horo/Runtime/Save/SaveArchiveFraming.h
