46ac558d01e070b99e78ecfcff6f793722ba27a3
abdullah <60715190+abdullahbodur@users.noreply.github.com>
2026-09-12 10:46:03 +0200

    feat(extensions): add pipeline step extension point HORO-98 #98 [EXT-002.6] (#2791)

--- cmake/HoroPublicHeaderOwnership.cmake
+++ cmake/HoroPublicHeaderOwnership.cmake
@@ -514,6 +514,7 @@ horo_configure_target_header_boundary(HoroExtensions PUBLIC_HEADERS
     Horo/Extensions/ExtensionManifest.h
     Horo/Extensions/ExtensionModuleResolution.h
     Horo/Extensions/ExtensionMarketplace.h
+    Horo/Extensions/PipelineStepRegistry.h
     Horo/Extensions/ProjectValidatorRegistry.h
 )
 
